9e8c6cfd59b3af9652e3c499ea367e122cb27ee2
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-05-01 03:28:00 -0400

    Test and fix for ImportHelpers::import_urls

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -11,7 +11,9 @@ describe "import_urls" do
   
   let(:stream) { "http://www.google.com\njavascript:alert(a)\n" +
     "http://en.wikipedia.org/\nJean-Paul Sartre" }
-  it "should change the db"
+  it "should change the db" do
+    expect{ @dummy.import_urls(stream) }.to change { Node.count }
+  end 
   it "does not insert non-URLs into db" do
     expect{ @dummy.import_urls(stream) }.to change { Node.count }.by(2)
   end
