c68b74f5496b5372163eb68a1e6b5747bd86048a
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-05-01 03:06:01 -0400

    Shared 'is json' example, added edge cases for export actions

--- spec/export_spec.rb
+++ spec/export_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
+shared_examples 'an export file' do
+  describe "is application/json" do
+    subject { last_response.header["Content-Type"] }
+    it { should include "application/json" }
+  end
+  
+end
+
 describe "/export" do
+  before(:each) { get '/export' }
+  it_behaves_like "an export file"
+  it "contains all nodes"
 end
 
 describe "/export/:tag" do
+  before(:each) { get '/export/review' }
+  it_behaves_like "an export file"
+  it "only contains nodes with a given tag"
+  it "contains all nodes with a given tag"
+  it "contains nothing if tag non-existent"
 end
 
 describe "/export-except/:tag" do
+  before(:each) { get '/export-except/review' }
+  it_behaves_like "an export file"
+  it "contains no nodes with a given tag"
+  it "contains all nodes without a given tag"
+  it "contains all nodes if tag non-existent"
 end
-
