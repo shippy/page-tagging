d9b5bb02b9f3626eb84b118e909754a9685a3b20
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-05-04 22:54:52 -0400

    Implemented /export* tests

--- spec/export_spec.rb
+++ spec/export_spec.rb
@@ -1,31 +1,64 @@
 require 'spec_helper'
+require 'json'
+
+before(:all) do
+  10.times do
+    node = Node.create(url: 'localhost', tag: %w{other review}.sample)
+    node.save
+  end
+end
+
+after(:all) { Node.delete_all }
 
 shared_examples 'an export file' do
   describe "is application/json" do
     subject { last_response.header["Content-Type"] }
     it { should include "application/json" }
   end
-  
 end
 
 describe "/export" do
   before(:each) { get '/export' }
   it_behaves_like "an export file"
-  it "contains all nodes"
+  it "contains all nodes" do
+    last_response.body.should == Node.all.to_json
+  end
 end
 
 describe "/export/:tag" do
-  before(:each) { get '/export/review' }
+  before(:all) do
+    get '/export/review'
+    @json = JSON.parse(last_response.body)
+  end
+  
   it_behaves_like "an export file"
-  it "only contains nodes with a given tag"
-  it "contains all nodes with a given tag"
-  it "contains nothing if tag non-existent"
+  it "has no nodes with a different tag" do
+    @json.each do |node|
+      node.tag.should == 'review'
+    end
+  end
+  
+  it "contains all nodes with a given tag" do
+    @json.should == Node.where(tag: 'review').to_a
+  end
 end
 
 describe "/export-except/:tag" do
-  before(:each) { get '/export-except/review' }
+  before(:each) do
+    get '/export-except/review'
+    @json = JSON.parse(last_response.body)
+  end
   it_behaves_like "an export file"
-  it "contains no nodes with a given tag"
-  it "contains all nodes without a given tag"
-  it "contains all nodes if tag non-existent"
+  it "contains no nodes with a given tag" do
+    @json.each do |n|
+      n.tag.should != 'review'
+    end
+  end
+  it "contains all nodes without a given tag" do
+    @json.should == Node.where("tag != ?", 'review').to_a
+  end
+  it "contains all nodes if tag non-existent" do
+    get '/export-except/nonexistent'
+    JSON.parse(last_response.body).should == Node.all.to_a
+  end
 end
